0e67907bea04cfdbf6554e094039219cea07778d
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2023-11-22 13:04:08 +0000

    add tests for trying to GET, PUT or DELETE a non-existing book

--- api.test.js
+++ api.test.js
@@ -50,4 +50,52 @@ describe('Books Api', () => {
         done();
       });
   });
+
+  it('shold PUT an existing book', (done) => {
+    const bookUpdate = { id: bookId, title: "Updated Test Book", author: "Updated Test Author" };
+    chai.request(server)
+      .put(`/books/${bookId}`)
+      .send(bookUpdate)
+      .end((err, res) => {
+        expect(res, 'Responce status shoud be 200').to.have.status(200);
+        expect(res.body, 'res.body is an object').to.be.an('object');
+        expect(res.body.id, 'res.body has correct "id" property').to.be.equal(bookUpdate.id);
+        expect(res.body.title, 'res.body has correct "title" property').to.be.equal(bookUpdate.title);
+        expect(res.body.author, 'res.body has correct "author" property').to.be.equal(bookUpdate.author);
+        done();
+      });
+  });
+
+  it('should Delete a single book', (done) => {
+    chai.request(server)
+      .delete(`/books/${bookId}`)
+      .end((err, res) => {
+        expect(res, 'Responce status shoud be 204').to.have.status(204);
+        done();
+      });
+  });
+
+  it('should return 404 when trying to GET, PUT or DELETE a non-existing book', (done) => {
+    chai.request(server)
+      .get('/books/9999')
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+      });
+
+    const nonExistingBook = { id: "999", title: "Non-existing Book", author: "Non-existing Author" };
+    chai.request(server)
+      .put('/books/9999')
+      .send(nonExistingBook)
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+      });
+
+    chai.request(server)
+      .delete('/books/9999')
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        done();
+      });
+  });
+
 });
