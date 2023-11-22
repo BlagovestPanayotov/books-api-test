90ddf9a01be58b184a0a2ffa5c0c6a74c3b66569
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2023-11-22 12:14:22 +0000

    add tests for GET a single book

--- api.test.js
+++ api.test.js
@@ -36,4 +36,18 @@ describe('Books Api', () => {
         done();
       });
   });
+
+  it('should GET a single book', (done) => {
+    chai.request(server)
+      .get(`/books/${bookId}`)
+      .end((err, res) => {
+        expect(res, 'Responce status shoud be 200').to.have.status(200);
+        expect(res.body, 'res.body is an object').to.be.an('object');
+        expect(res.body, 'res.body have a property "id"').to.have.property('id');
+        expect(res.body, 'res.body have a property "title"').to.have.property('title');
+        expect(res.body, 'res.body have a property "author"').to.have.property('author');
+        expect(res.body.id, 'res.body has to be correct').to.be.equal(bookId);
+        done();
+      });
+  });
 });
