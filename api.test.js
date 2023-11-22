05b47738d36cb2cf2966315a12e85ad4025b0ce9
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2023-11-22 11:28:24 +0000

    add test for post a book

--- api.test.js
+++ api.test.js
@@ -6,3 +6,25 @@ const server = require('./server');
 
 
 chai.use(chaiHttp);
+
+
+describe('Books Api', () => {
+  let bookId;
+  //Verify Posting a Book
+
+  it('should POST a book', (done) => {
+    const book = { id: "1", title: "Test Book", author: "Test Author" };
+    chai.request(server)
+      .post('/books')
+      .send(book)
+      .end((err, res) => {
+        expect(res,'Responce statust shoud be 201').to.have.status(201);
+        expect(res.body,'res.body is an object').to.be.an('object');
+        expect(res.body,'res.body have a property "id"').to.have.property('id');
+        expect(res.body,'res.body have a property "title"').to.have.property('title');
+        expect(res.body,'res.body have a property "author"').to.have.property('author');
+        bookId = res.body.id;
+        done();
+      });
+  });
+});
