206ce78e5135de62404283e43b8b6102fba92983
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2023-11-22 12:09:05 +0000

    add test for getting all books

--- api.test.js
+++ api.test.js
@@ -10,7 +10,6 @@ chai.use(chaiHttp);
 
 describe('Books Api', () => {
   let bookId;
-  //Verify Posting a Book
 
   it('should POST a book', (done) => {
     const book = { id: "1", title: "Test Book", author: "Test Author" };
@@ -18,12 +17,22 @@ describe('Books Api', () => {
       .post('/books')
       .send(book)
       .end((err, res) => {
-        expect(res,'Responce statust shoud be 201').to.have.status(201);
-        expect(res.body,'res.body is an object').to.be.an('object');
-        expect(res.body,'res.body have a property "id"').to.have.property('id');
-        expect(res.body,'res.body have a property "title"').to.have.property('title');
-        expect(res.body,'res.body have a property "author"').to.have.property('author');
+        expect(res, 'Responce statust shoud be 201').to.have.status(201);
+        expect(res.body, 'res.body is an object').to.be.an('object');
+        expect(res.body, 'res.body have a property "id"').to.have.property('id');
+        expect(res.body, 'res.body have a property "title"').to.have.property('title');
+        expect(res.body, 'res.body have a property "author"').to.have.property('author');
         bookId = res.body.id;
+        done();
+      });
+  });
+
+  it('should GET all books', (done) => {
+    chai.request(server)
+      .get('/books')
+      .end((err, res) => {
+        expect(res, 'Responce status shoud be 200').to.have.status(200);
+        expect(res.body, 'res.body is array').to.be.an('array');
         done();
       });
   });
